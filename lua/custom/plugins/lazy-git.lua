17dfe5912e0197cb0d9404113f0046ecd071037c
Aldeon Moriak <aldeonmoriak@gmail.com>
2026-07-01 09:42:27 +0330

    fix lazygit and oil plugins

--- lua/custom/plugins/lazy-git.lua
+++ lua/custom/plugins/lazy-git.lua
@@ -1,22 +1,24 @@
-return {
-  {
-    'kdheepak/lazygit.nvim',
-    lazy = true,
-    cmd = {
-      'LazyGit',
-      'LazyGitConfig',
-      'LazyGitCurrentFile',
-      'LazyGitFilter',
-      'LazyGitFilterCurrentFile',
-    },
-    -- optional for floating window border decoration
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-    },
-    -- setting the keybinding for LazyGit with 'keys' is recommended in
-    -- order to load the plugin when the command is run for the first time
-    keys = {
-      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
-    },
-  },
+vim.pack.add {
+  'https://github.com/kdheepak/lazygit.nvim',
 }
+require('telescope').load_extension('lazygit')
+-- require('lazygit').setup {
+--     cmd = {
+--       'LazyGit',
+--       'LazyGitConfig',
+--       'LazyGitCurrentFile',
+--       'LazyGitFilter',
+--       'LazyGitFilterCurrentFile',
+--     },
+--     -- optional for floating window border decoration
+--     dependencies = {
+--       'nvim-lua/plenary.nvim',
+--     },
+--     -- setting the keybinding for LazyGit with 'keys' is recommended in
+--     -- order to load the plugin when the command is run for the first time
+--     keys = {
+--       { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+--     },
+--     lazy = true,
+-- }
+vim.keymap.set('n', '<leader>lg', '<cmd>LazyGit<cr>', { desc = 'LazyGit' })
--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -1,30 +1,18 @@
-return {
-  {
-    'stevearc/oil.nvim',
-    ---@module 'oil'
-    ---@type oil.SetupOpts
-    opts = {},
-    -- Optional dependencies
-    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
-    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
-    config = function()
-      require('oil').setup {
-        columns = { 'icon' },
-        view_options = {
-          show_hidden = true,
-        },
-        git = {
-          mv = function(src_path, dest_path)
-            return true
-          end,
-        },
-      }
-
-      -- Open parent directory in current window
-      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
-
-      -- Open parent directory in floating window
-      vim.keymap.set('n', '<leader>-', require('oil').toggle_float, { desc = 'Open parent directory in floating window' })
-    end,
+vim.pack.add {
+  'https://github.com/stevearc/oil.nvim',
+}
+require('oil').setup {
+  columns = { 'icon' },
+  view_options = {
+    show_hidden = true,
+  },
+  git = {
+    mv = function(src_path, dest_path) return true end,
   },
 }
+
+-- Open parent directory in current window
+vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+
+-- Open parent directory in floating window
+vim.keymap.set('n', '<leader>-', require('oil').toggle_float, { desc = 'Open parent directory in floating window' })
